66bc57d5f4c7c18f4b5eb983acadac56872d4dfe
Nathan Sepulveda <nathans@thephpeffect.com>
2015-08-20 11:22:37 -0700

    TriggrTest class touched

---
+++ tests/TriggrTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace TPE\TriggrPHP\Test;
+
+/**
+ * The Triggr test class
+ */
+class TriggrTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTriggr()
+    {
+        
+    }
+}
